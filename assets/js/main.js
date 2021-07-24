a0254a95ad69952fa2d3f1c7bff4ca3b20ceac7c
Gabriela Menezes <gabimenezess10@gmail.com>
2021-07-24 20:12:09 -0300

    adding variables

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+let minutes = 25;
+let seconds = 00;
+
+const minutesEl = document.getElementById('minutes')
+const secondsEl = document.getElementById('seconds')
+
+
+minutesEl.innerHTML = minutes;
+secondsEl.innerHTML = seconds;
+
+function startTimer() {
+    
+    setInterval(() => {
+        
+    }, 1000)
+}
