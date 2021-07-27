3db38e99a029934ffa2715c22020d94d73fe1f97
Gabriela Menezes <gabrielamendes.1611@aluno.una.br>
2021-07-27 11:25:28 -0300

    + event.preventDefault()

--- assets/js/Modal.js
+++ assets/js/Modal.js
@@ -1,6 +1,6 @@
 const modal = {
-    open(){
-        
+    open(event){
+        event.preventDefault();
         //abrir modal
         //adicionar a classe active ao modal
         document.querySelector('.modal-overlay').classList.add('active')
