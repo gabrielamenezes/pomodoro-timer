5836d500746d5f3c4e2fee224cbbd9a19e35d3ce
Gabriela Menezes <gabimenezess10@gmail.com>
2021-07-27 08:32:51 -0300

    feature: modal funcionalidade add

---
+++ assets/js/Modal.js
@@ -0,0 +1,15 @@
+const modal = {
+    open(){
+        
+        //abrir modal
+        //adicionar a classe active ao modal
+        document.querySelector('.modal-overlay').classList.add('active')
+    },
+
+    close() {
+        //fechar modal
+        //remover a classe active do modal
+
+        document.querySelector('.modal-overlay').classList.remove('active')
+    }
+};
