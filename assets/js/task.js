56676922b314d6f312dab99414ddfbaa8cc68f27
Gabriela Menezes <gabimenezess10@gmail.com>
2021-07-28 08:42:21 -0300

    logica da task

---
+++ assets/js/task.js
@@ -0,0 +1,27 @@
+// criando a função para criar as tarefas
+
+/*<label class="todo__item">
+    <input type="checkbox">
+    <div>teste de item 2</div>
+    <button class="delete"><i class="fas fa-trash-alt delete"></i></button>
+</label>*/
+
+let bancoDeDados = [
+
+];
+const createTask = (task, status='') => {
+    const item = document.createElement('label') //criando o label
+    item.classList.add('todo__item');
+    item.innerHTML = `
+        <input type="checkbox ${status}">
+        <div>${task}</div> 
+        <button class="delete"><i class="fas fa-trash-alt delete"></i></button>
+    `
+
+    document.querySelector('.todo__list').appendChild(item)
+}
+
+const render = () => {
+    clearTask()
+    bancoDeDados.forEach(task => createTask(task.name))
+}
