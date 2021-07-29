9e619a452a38dc6efb9bdffffa5106402c7035c4
Gabriela Menezes <gabimenezess10@gmail.com>
2021-07-28 22:06:12 -0300

    chamada da função render e criação da function que simula um BD

--- assets/js/task.js
+++ assets/js/task.js
@@ -7,13 +7,15 @@
 </label>*/
 
 let bancoDeDados = [
+    {'name': 'Estudar javascript', 'status': ''},
+    {'name': 'Estudar CSS', 'status': 'checked'}
 
 ];
 const createTask = (task, status='') => {
     const item = document.createElement('label') //criando o label
     item.classList.add('todo__item');
     item.innerHTML = `
-        <input type="checkbox ${status}">
+        <input type="checkbox" ${status}>
         <div>${task}</div> 
         <button class="delete"><i class="fas fa-trash-alt delete"></i></button>
     `
@@ -22,6 +24,8 @@ const createTask = (task, status='') => {
 }
 
 const render = () => {
-    clearTask()
-    bancoDeDados.forEach(task => createTask(task.name))
+    
+    bancoDeDados.forEach(task => createTask(task.name, task.status))
 }
+
+render();
