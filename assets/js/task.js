7c61c2c8048145c4875a8114a3f1ad8943d86085
Gabriela Menezes <gabimenezess10@gmail.com>
2021-07-30 07:36:37 -0300

    inserção da função onClickTask + removeTask

--- assets/js/task.js
+++ assets/js/task.js
@@ -8,13 +8,13 @@
 
 let dataBase = [
 ];
-const createTask = (task, status='') => {
+const createTask = (task, status='', indice) => {
     const item = document.createElement('label') //criando o label
     item.classList.add('todo__item');
     item.innerHTML = `
-        <input type="checkbox" ${status}>
+        <input type="checkbox" ${status} data-indice=${indice}>
         <div>${task}</div> 
-        <button class="delete"><i class="fas fa-trash-alt delete"></i></button>
+        <button class="delete"><i class="fas fa-trash-alt delete" data-id="delete-icon" data-indice=${indice}></i></button>
     `
 
     document.querySelector('.todo__list').appendChild(item)
@@ -30,8 +30,9 @@ const clearTask = () =>  {
 //atualizar a tela com as informações do database
 const render = () => {
     clearTask();
-    dataBase.forEach(task => createTask(task.name, task.status))
+    dataBase.forEach((task, indice) => createTask(task.name, task.status, indice))
     //para cada obj no db, vao ser criadas task
+    //indice para poder diferenciar cada um dos itens
 }
 
 const addTask = (event) => {
@@ -42,5 +43,19 @@ const addTask = (event) => {
     input.value = '';
     modal.close(); //fechar modal
 }
+
+const removeTask = indice => {
+    dataBase.splice(`${indice}, 1`)//deletando 1 item a partir do indice enviado por parâmetro
+    render();
+} 
+const onClickTask = (event) => {
+    const el = event.target;
+    
+    if(el.getAttribute('data-id') === 'delete-icon') {
+        const indice = el.dataset.indice
+        removeTask(indice)
+    }
+}
+document.getElementById('todoList').addEventListener('click', onClickTask)
 document.getElementById('addTask').addEventListener('click', addTask)
 render();
