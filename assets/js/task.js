3c4b5774cec69a37d633e33a358e98ef8668a2d5
Gabriela Menezes <gabimenezess10@gmail.com>
2021-07-30 16:11:12 -0300

    localStorage add

--- assets/js/task.js
+++ assets/js/task.js
@@ -6,8 +6,11 @@
     <button class="delete"><i class="fas fa-trash-alt delete"></i></button>
 </label>*/
 
-let dataBase = [
-];
+//let dataBase = [];
+//se tiver vazio, retorna 1 array vazio senão pega do localStorage o item todoList
+const getdb = () => JSON.parse(localStorage.getItem('todoList')) ?? [];
+const setdb = dataBase => localStorage.setItem('todoList', JSON.stringify(dataBase))
+
 const createTask = (task, status='', indice) => {
     const item = document.createElement('label') //criando o label
     item.classList.add('todo__item');
@@ -30,27 +33,35 @@ const clearTask = () =>  {
 //atualizar a tela com as informações do database
 const render = () => {
     clearTask();
-    dataBase.forEach((task, indice) => createTask(task.name, task.status, indice))
+    const dataBase = getdb();
+    //dataBase.forEach((task, indice) => createTask(task.name, task.status, indice))
     //para cada obj no db, vao ser criadas task
     //indice para poder diferenciar cada um dos itens
 }
 
 const addTask = (event) => {
     event.preventDefault();
+    const dataBase = getBanco();
     const input = document.getElementById('titleTask')
     dataBase.push({'name': input.value, 'status': '' })
+
+    setdb(dataBase)
     render();
     input.value = '';
     modal.close(); //fechar modal
 }
 
 const removeTask = indice => {
+    const dataBase = getdb();
     dataBase.splice(`${indice}, 1`)//deletando 1 item a partir do indice enviado por parâmetro
+    setdb(dataBase) //atualiza localStorage
     render();
 }
 
 const updateDataBase = indice => {
+    const dataBase = getdb();
     dataBase[indice].status = dataBase[indice].status === '' ? 'checked' : '';
+    setdb(dataBase)
     render();
 } 
 const onClickTask = (event) => {
